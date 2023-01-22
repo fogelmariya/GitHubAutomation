6daeacd27bf3627c55cd2ffea5fb19d042f2a17a
Mariia_Fogel <fogelmariya@gmail.com>
2023-01-22 14:18:48 +0200

    123

---
+++ github-automation-master/java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -0,0 +1,33 @@
+package com.epam.ta.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSingleton {
+
+    private static WebDriver driver;
+
+
+    private DriverSingleton(){}
+
+    public static WebDriver getDriver(){
+        System.setProperty("browser", "chrome");
+        if (null == driver){
+            if ("firefox".equals(System.getProperty("browser"))) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        driver.quit();
+        driver = null;
+    }
+}
